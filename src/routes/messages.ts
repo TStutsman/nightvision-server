196cd615a1fe685b4c9844677271b28a9f299a10
TStutsman <teagandstutsman@gmail.com>
2024-11-21 16:00:51 -0600

    change: refactored broadcast function in gameService to handle arrays of reactions as well

--- src/routes/messages.ts
+++ src/routes/messages.ts
@@ -4,52 +4,40 @@ import { JSONEventRouter } from "../service/EventRouter.js";
 const messages = new JSONEventRouter();
 
 messages.on('tileClick', (event, playerService) => {
-    if(!event.data || event.data.tileId === undefined) return new ClientError('Must provide tile id for tileClick action');
+    if(!event.data || event.data.tileId === undefined) {
+        playerService.json(new ClientError('Must provide tile id for tileClick action'));
+        return;
+    }
 
     const { game, id } = playerService;
 
     const reactions = game.tileClick(id, event.data.tileId);
 
-    for(const reaction of reactions) {
-        game.broadcast(reaction);
-    }
-    return;
+    game.broadcast(reactions);
 });
 
 messages.on('bearSpray', (_, playerService) => {
     const reactions = playerService.game.buySpray(playerService.id);
 
-    for(const reaction of reactions) {
-        playerService.game.broadcast(reaction);
-    }
-    return;
+    playerService.game.broadcast(reactions);
 });
 
 messages.on('reshuffle', (_, playerService) => {
     const reactions = playerService.game.reshuffle(playerService.id);
 
-    for(const reaction of reactions) {
-        playerService.game.broadcast(reaction);
-    }
-    return;
+    playerService.game.broadcast(reactions);
 });
 
 messages.on('flashlight', (_, playerService) => {
     const reactions = playerService.game.turnOnFlashlight(playerService.id);
 
-    for(const reaction of reactions) {
-        playerService.game.broadcast(reaction);
-    }
-    return;
+    playerService.game.broadcast(reactions);
 });
 
 messages.on('playAgain', (_, playerService) => {
     const reactions = playerService.game.resetGame();
 
-    for(const reaction of reactions) {
-        playerService.game.broadcast(reaction);
-    }
-    return;
+    playerService.game.broadcast(reactions);
 });
 
 export { messages as messageRouter };
--- src/service/GameService.ts
+++ src/service/GameService.ts
@@ -54,11 +54,34 @@ export class GameService extends NightVisionGame {
     }
 
     /**
-     * Sends a Reaction as JSON to all clients registered to this game
+     * Sends one Reaction as JSON to all clients registered to this game
      * 
      * @param reaction - a Reaction object to emit
      */
-    broadcast(reaction: Reaction):void {
+    broadcast(reaction: Reaction):void;
+    /**
+     * Sends each Reaction in an array to all clients registered to this game
+     * 
+     * @param reactions - an array of Reaction objects to emit
+     */
+    broadcast(reactions: Reaction[]):void;
+    broadcast(reactionOrReactions: Reaction | Reaction[]):void {
+        if(reactionOrReactions instanceof Reaction){
+            return this._broadcast(reactionOrReactions);
+        }
+
+        for(const reaction of reactionOrReactions){
+            this._broadcast(reaction);
+        }
+    }
+    
+    /**
+     * Sends a single Reaction as JSON to all clients registered to this game
+     * Used internally by broadcast()
+     * 
+     * @param reaction - a Reaction object to emit
+     */
+    _broadcast(reaction:Reaction):void {
         for(const token in this.clients){
             const client = this.clients[token];
             client.ws.send(reaction.json());
